97817dc23ac435a32e43344f451dd6bb5bf8e882
BharathMopuru <95495923+BharathMopuru@users.noreply.github.com>
2022-11-16 10:57:28 -0600

    update is added

--- routes/dog.js
+++ routes/dog.js
@@ -30,6 +30,9 @@ router.get('/dog/:id', dog_controllers.dog_detail);
 router.get('/detail', dog_controllers.dog_view_one_Page);
 /* GET create dog page */ 
 router.get('/create', dog_controllers.dog_create_Page); 
+/* GET create update page */ 
+router.get('/update', dog_controllers.dog_update_Page); 
+ 
 
 module.exports = router;
 
